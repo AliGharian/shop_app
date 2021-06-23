daa701fbc4edb8d3c5ab3173141254d46b03826c
aligharian <ali.gharian98@gmail.com>
2021-06-23 11:24:11 +0430

    add details to user info page along with sliver app bar

---
+++ lib/consts/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class KColors {
+  static Color black = Color(0xFF000000);
+  static Color white = Color(0xFFFFFFFF);
+
+  static Color title = Color(0xDD000000);
+  static Color subTitle = Color(0x8A000000);
+}
